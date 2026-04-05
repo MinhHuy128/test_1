021958d501d8c9e8c78fabe261945b573e695dde
HuyMinh <151420337+MinhHuy128@users.noreply.github.com>
2026-04-05 11:08:30 +0700

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('iot-store').then((cache) => cache.addAll([
-      'index.html',
-      'manifest.json',
-    ])),
-  );
+  e.waitUntil(caches.open('iot-v1').then((cache) => cache.addAll(['index.html', 'manifest.json'])));
 });
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
